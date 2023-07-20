201365eec1492758524d2bc470e49e01f9f928df
Muhammad  Moid Shams <me_moid@hotmail.com>
2023-07-20 16:53:09 +0500

    add

---
+++ src/app/modules/admin/file-manager/create-or-edit/create-or-edit.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-or-edit',
+  templateUrl: './create-or-edit.component.html',
+  styleUrls: ['./create-or-edit.component.scss']
+})
+export class CreateOrEditComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
